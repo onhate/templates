1611979fb4f55b9a88b7499a9ee0f6a0a4b62bcc
ChaimP <chaim.platonov@env0.com>
2024-03-04 10:46:07 +0200

    cdk example

---
+++ aws/cdk/lib/cdk-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+            versioned: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+    }
+}
