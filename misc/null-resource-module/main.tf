7f10ed7485dd77749e30ac811c9a92ff3639fca2
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2024-12-17 14:36:55 -0300

    Update main.tf

--- misc/null-resource-module/main.tf
+++ misc/null-resource-module/main.tf
@@ -1,4 +1,4 @@
 module "my-module" {
-  source = "api.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/null/null"
+  source = "api-pr17656.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/null/null"
   version = "0.0.1"
 }
